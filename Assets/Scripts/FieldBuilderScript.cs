c193ef2ce460b1f5fe1c6f31c828be0a3c77c8f6
xSethrin <62115192+xSethrin@users.noreply.github.com>
2020-04-23 13:53:58 -0500

    some files were untracked.  fixed it

---
+++ Assets/Scripts/FieldBuilderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldBuilderScript : MonoBehaviour{
+
+    public Transform fieldObject; 
+
+    //TODO
+    //public feildBehavior [] field;//need to initiate as untilled and unwatered.
+
+    // Start is called before the first frame update
+    void Start(){
+        //TODO
+        //make if stament that checks saved data for field.  If it is there instead of making an empty field we can update it to the aved file then paint it.
+        //this should be done every game day at the start of the day.
+        //this should look for the isWatered.  If it iswatered update the stage and sprite before painting
+        for(int xPos = -1 ; xPos < 14; xPos++){
+            for(int yPos = -14; yPos > -23; yPos--){
+                Instantiate (fieldObject, new Vector2(xPos, yPos), fieldObject.rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
---
+++ Assets/Scripts/fieldBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fieldBehavior : MonoBehaviour{
+
+    public Sprite tilled, stage1, stage1W, stage2, stage2W, stage3;//each of these is used to update the sprite
+    public int timeWatered = 0, count = 0;//timesWatered is used to detemer how many times the plant was watered.  this value will be used to check plants stage as well.
+    //count is a temp value for testing, will remove later
+    public bool isWatered;//isWatered checks if the plant was watered.  This will be used when player goes to sleep to properly update plant
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    
+    void OnMouseDown(){
+        //TODO
+        //we will not allow people to water tilled fields with no plant.
+        //we will not allow people to plant in untilled soil
+        //make sure to check these things in if statments
+
+        if(count == 0){//change to hoe equipted
+            Debug.Log("Field was tilled!");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = tilled;
+        }
+        else if(count == 1){//change to seed equipted
+            Debug.Log("Seed planted! at stage 1");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = stage1;
+        }
+        else if (count == 2){
+            Debug.Log("Stage 1 watered");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = stage1W;
+        }
+        else if(count == 3){
+            Debug.Log("plant grew! at stage 2");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = stage2;
+        }
+         else if(count == 4){
+            Debug.Log("stage two watered watered");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = stage2W;
+        }
+         else if(count == 5){
+            Debug.Log("plant grew! at stage 3");
+            count++;
+            GetComponent<SpriteRenderer>().sprite = stage3;
+        }
+         else if(count == 6){
+            Debug.Log("harvested!");
+            count = 0;
+            GetComponent<SpriteRenderer>().sprite = tilled;
+         }
+    }
+    
+}
