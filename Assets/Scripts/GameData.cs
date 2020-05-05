e82fb0d856baa6f1e5f2cb86fca37112f942e9f6
xSethrin <62115192+xSethrin@users.noreply.github.com>
2020-05-05 10:43:27 -0500

    added all the new scripts and stuff

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+* This class gatheres all data needed to be saved and store it in a single object
+* That object is then saved to an external file in binary form
+*/
+[System.Serializable]//this allows the object to be converted into a binary form and saved to a file
+public class GameData {
+
+    public int [] playerData = new int [6];//int array for Player data
+    public int [] hasCow;//int array for cow bool
+    public int [] hasMilk;//int array for milk bool
+    public int [] hasChicken;//int array for chicken bool
+    public int [] hasEgg;//int array for egg bool
+    public int [] currentStage;
+    public int [] timesWatered;
+    public int [] isTilled;
+    public int [] hasPlant;
+
+    /** 
+    * This is a constructor for the GameData object 
+    * This constructor is used when we need to grab data from the game objects to store into the game data
+    * it requires GameObject player, GameObject [] cows, GameObject [] chickens and GameObject [] plants
+    * each objected will be parsed and data from these objects that needs to be saved will be stored
+    */
+    public GameData(GameObject player, GameObject [] cows, GameObject [] chickens, GameObject [] plants){
+        JackObject jack = player.GetComponent<JackObject>();//grabs the players data
+        //these lines set the players data to the array         
+        playerData[0] = jack.gold;
+        playerData[1] = jack.water;   
+        playerData[2] = jack.hoe;
+        playerData[3] = jack.sickle;
+        playerData[4] = jack.hammer;
+        playerData[5] = jack.axe;
+        hasCow = new int [cows.Length];//instantiates hasCow array 
+        hasMilk = new int [cows.Length];//instantiate hasMilk array
+        for(int i = 0; i < cows.Length; i++){//loops through the cows
+            CowBehavior cow = cows[i].GetComponent<CowBehavior>();//grabs cow data
+            if(cow.hasCow){//checks if has cow
+                hasCow[i] = 1;
+            }
+            else{
+                hasCow[i] = 0;
+            }
+            if(cow.hasMilk){//check if has milk
+                hasMilk[i] = 1;
+            }
+            else{
+                hasMilk[i] = 0;
+            }
+        }
+        hasChicken = new int [chickens.Length];//instantiate hasChicken array
+        hasEgg = new int [chickens.Length];//instantiate hasEgg array
+        for(int i = 0; i < chickens.Length; i++){//this loops through the chickens
+             ChickenBehavior chicken = chickens[i].GetComponent<ChickenBehavior>();//grabs chicken data
+             if(chicken.hasChicken){//checks if has chicken
+                 hasChicken[i] = 1;
+             }
+             else{
+                 hasChicken[i] = 0;
+             }
+             if(chicken.hasEgg){//checks if has egg
+                 hasEgg[i] = 1;
+             }
+             else{
+                 hasEgg[i] = 0;
+             }
+        }
+        currentStage = new int [plants.Length];//instantiate currentStage array
+        timesWatered = new int [plants.Length];//instantiate timesWatered array
+        isTilled = new int [plants.Length];//instantiate isTilled array
+        hasPlant = new int [plants.Length];//instantiate hasPlant array
+        for(int i = 0; i < plants.Length; i++){//loops through plant arrays
+            PlantBehavior plant = plants[i].GetComponent<PlantBehavior>();//grabs plant data
+            currentStage[i] = plant.currentStage;//sets current stage for plant
+            timesWatered[i] = plant.timesWatered;//sets times watered for plant
+            if(plant.isTilled){//sets value for tilled
+                isTilled[i] = 1;
+            }
+            else{
+                isTilled[i] = 0;
+            }
+            if(plant.hasPlant){//sets value for hasplant
+                hasPlant[i] = 1;
+            }
+            else{
+                hasPlant[i] = 0;
+            }
+        }
+    } 
+
+    /**
+    * This constructor is used to load data
+    *
+    */
+    public GameData(int [] playerData, int [] hasCow, int [] hasMilk, int [] hasChicken,int [] hasEgg, int [] currentStage, int [] timesWatered, int [] isTilled, int [] hasPlant){
+        this.playerData = playerData;
+        this.hasCow = hasCow;
+        this.hasMilk = hasMilk;
+        this.hasChicken = hasChicken;
+        this.hasEgg = hasEgg;
+        this.currentStage = currentStage;
+        this.timesWatered =timesWatered;
+        this.isTilled = isTilled;
+        this.hasPlant = hasPlant;
+    }
+}
+
---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* Thios class runs the load function.
+* It takes the binary file and changes it back to primative data types
+* This data is then put back into their game objects
+*/
+public class LoadGame : MonoBehaviour{
+
+    /**
+    *This method is run when the player clicks the load button
+    *It loads in all data from the most recent save
+    */
+    void OnMouseDown(){
+        SaveSystem save = new SaveSystem();//creates a save object
+        GameData data = save.LoadGame();//creates a game data object
+        int [] playerData = data.playerData;//int array for Player data
+        int [] hasCow = data.hasCow;//int array for cow bool
+        int [] hasMilk = data.hasMilk;//int array for milk bool
+        int [] hasChicken = data.hasChicken;//int array for chicken bool
+        int [] hasEgg = data.hasEgg;//int array for egg bool
+        int [] currentStage = data.currentStage;
+        int [] timesWatered = data.timesWatered;
+        int [] isTilled = data.isTilled;
+        int [] hasPlant = data.hasPlant;
+        GameObject [] players = GameObject.FindGameObjectsWithTag("Player");//finds the player
+        JackObject jack = players[0].GetComponent<JackObject>();//gets the JackObject component
+        jack.stam = 100;//restes jacks stamina
+        //GameObject [] plants = GameObject.FindGameObjectsWithTag("Plant");//gets the plant objects
+        GameObject [] cows = GameObject.FindGameObjectsWithTag("Cow");//gets the cow objects
+        GameObject [] chickens = GameObject.FindGameObjectsWithTag("Chicken");//gets the chicken objects
+        GameObject [] plants = GameObject.FindGameObjectsWithTag("Plant");//gets all plant objects
+        //the following lines set all of jacks data back to what was saved
+        jack.gold = playerData[0];
+        jack.water = playerData[1];   
+        jack.hoe = playerData[2];
+        jack.sickle = playerData[3];
+        jack.hammer = playerData[4];
+        jack.axe = playerData[5];
+        int i = 0;//variable for incrimenting through the cow, chicken, and plant objects
+        foreach(GameObject cow in cows){//loop through each cow object
+            CowBehavior cowData = cow.GetComponent<CowBehavior>();//grabs the cow behavior which holds cow data
+            if(hasCow[i] == 1){//checks if has cow is true
+                cowData.hasCow = true;//sets to true
+            }
+            else{
+                cowData.hasCow = false;//set false
+            }
+            if(hasMilk[i] == 1){//checks if cow has milk
+                cowData.hasMilk = true;//sets true
+            }
+            else{
+                cowData.hasMilk = false;//sets false
+            }
+            cowData.wasFed = false;//wasFed set is always set to false. THIS IS NEEDED TO PREVENT BUGS IF PLAYER LOADS GAME WHILE PLAYING WITHOUT SAVING 1ST
+            cowData.UpdateSprite();//updates cow sprite
+            i++;
+        }
+        i = 0;//resest i for chickens
+        foreach(GameObject chicken in chickens){
+            ChickenBehavior chickenData = chicken.GetComponent<ChickenBehavior>();//grab chicken behavior
+            if(hasChicken[i] == 1){//check if has chicken is true
+                chickenData.hasChicken = true;
+            }
+            else{
+                chickenData.hasChicken = false;
+            }
+            if(hasEgg[i] == 1){//check if has egg is true
+                chickenData.hasEgg = true;
+            }
+            else{
+                chickenData.hasEgg = false;
+            }
+            chickenData.wasFed = false;//always set to false, needed to prevent bugs
+            chickenData.UpdateSprite();//updates sprite
+            i++;
+        }
+        i = 0;//restes i for plants
+        foreach(GameObject plant in plants){
+            PlantBehavior plantData = plant.GetComponent<PlantBehavior>();
+            plantData.currentStage = currentStage[i];//sets current stage for plant
+            plantData.timesWatered = timesWatered[i];//sets times watered for plant
+            if(isTilled[i] == 1){//sets value for tilled
+                plantData.isTilled = true;
+            }
+            else{
+                plantData.isTilled = false;
+            }
+            if(hasPlant[i] == 1){//sets value for hasplant
+                plantData.hasPlant = true;
+            }
+            else{
+                plantData.hasPlant = false;
+            }
+            plantData.isWatered = false;
+            plantData.UpdateSprite(currentStage[i]);
+        }
+    }
+}
