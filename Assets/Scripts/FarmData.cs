8e6352f956b7f9807ed06592b138e9a8a95f8be3
xSethrin <62115192+xSethrin@users.noreply.github.com>
2020-05-12 14:42:52 -0500

    added title screen working on static global data

---
+++ Assets/Scripts/FarmData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FarmData{
+
+    public static int [] timesWatered;
+    public static int [] currentStage;
+    public static bool [] isWatered;
+    public static bool [] isTilled;
+    public static bool [] hasPlant;
+    public static bool [] hasCow;
+    public static bool [] cowWasFed;
+    public static bool [] hasMilk;
+    public static bool [] hasChicken;
+    public static bool [] chickenWasFed;
+    public static bool [] hasEgg;
+
+    //need to delete these
+    public static GameObject [] plantData;
+    public static GameObject [] cowData;
+    public static GameObject [] chickenData;
+  
+}
---
+++ Assets/Scripts/Scene_Managers/TestStatics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStatics : MonoBehaviour
+{
+
+    void OnMouseDown(){
+        JackObject.stam-=5;
+        Debug.Log("Stam: " + JackObject.stam);
+        /**
+        Debug.Log("Test object clicked");
+        if(FarmData.chickenData != null){
+            Debug.Log("Chicken Objects: " + FarmData.chickenData.Length);
+        }
+        else{
+            Debug.Log("Chicken Objects is null");
+        }
+        if(FarmData.plantData != null){
+            Debug.Log("Plant Objects: " + FarmData.plantData.Length);
+        }
+        else{
+            Debug.Log("Plant Objects is null ");
+        }
+        if(FarmData.cowData != null){
+            Debug.Log("Cow Objects: " + FarmData.cowData.Length);
+        }
+        else{
+            Debug.Log("Cow Objects is null ");
+        }
+        */
+        
+    }
+}
