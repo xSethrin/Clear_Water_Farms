ee5a65e3f06b1906ada0f1736dbee4fbdc491411
Sterling <Rohlinsm6519@uwec.edu>
2020-05-12 15:29:22 -0500

    I have added the changing scene stuff.

---
+++ Assets/Scripts/Title_Load_ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_Load_ChangeScene : MonoBehaviour
+{
+    [SerializeField] private string toScene = null;
+
+    private SceneController sceneController;
+    void Start()
+    {
+        sceneController = GameObject.FindGameObjectWithTag("GameController").GetComponent<SceneController>();
+    }
+    //Nikolo you will have to put your code inside of update above the sceneController line. This 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sceneController.LoadScene(toScene);//loading new scene
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            sceneController.LoadScene(toScene);//loading new scene
+        }
+
+    }
+}
+
