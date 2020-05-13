b6ce04ec8111035e84459cd0722fedb05b67e13d
knappke4194 <knappke4194@uwec.edu>
2020-05-13 13:16:48 -0500

    meh

--- Assets/Scripts/JackObject.cs
+++ Assets/Scripts/JackObject.cs
@@ -8,5 +8,7 @@ public static class JackObject{
     public static int stam = 150, gold = 500, water = 0, hoe = 0, sickle = 0, hammer = 0, axe = 0, ore = 0, lumber = 0, seeds = 0, fodder = 0, waterAmount = 0;
     //public bool[] isFull = new bool[6];//dont need?
     public static List<Produce> slots = new List<Produce>();
+
+
 }
 //
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,16 @@ public class PlayerController : MonoBehaviour{
 public float speed = 10.4f;
 public Animator animator;
 public GameObject[] tools;
+public int currentHealth;
+public int maxHealth = 150;
+public HealthBar healthBar;
 
-void Update(){
+    private void Start() {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update(){
     
     Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
     animator.SetFloat("Horizontal", movement.x);
@@ -46,11 +54,15 @@ void Update(){
         if (Input.GetKey("6")){
         JackObject.currentTool = "axe";
         Debug.Log("Switched to axe");
-
+            ReduceStamina(20);
         }
     }
 
-    
+    public void ReduceStamina(int stamina) {
+        currentHealth -= stamina;
+        healthBar.SetHealth(currentHealth);
+    }
+
     /**
     Vector3 pos = transform.position;
 
@@ -71,94 +83,94 @@ void Update(){
 }
 */
 
-/**
+    /**
 
 ALL THIS CODE IS WHAT WE HAD WHEN WE WERE USING A CONTOLLER
 
-    private new Rigidbody2D rigidbody;
-    Vector2 velocity;// = new Vector2();
-    string buffer;
-    public GameObject prefab;
-    private SerialPort serial;
-    public Animator animator;
-    //public GameObject circle;
-    // Start is called before the first frame update
-    void Start(){
-        serial = new SerialPort("COM4", 9600);
-        serial.Open();  
-        rigidbody = GetComponent<Rigidbody2D>();
-        velocity = new Vector2(0,0);
-        serial.ReadTimeout = 1;
+        private new Rigidbody2D rigidbody;
+        Vector2 velocity;// = new Vector2();
+        string buffer;
+        public GameObject prefab;
+        private SerialPort serial;
+        public Animator animator;
+        //public GameObject circle;
+        // Start is called before the first frame update
+        void Start(){
+            serial = new SerialPort("COM4", 9600);
+            serial.Open();  
+            rigidbody = GetComponent<Rigidbody2D>();
+            velocity = new Vector2(0,0);
+            serial.ReadTimeout = 1;
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update(){
-        //string state = serial.ReadLine();
-        try{
-            buffer += serial.ReadExisting();
-            //SDebug.Log(buffer);
-            int index0 = buffer.LastIndexOf(']');
-            if(index0 >= 0){
-                int index1 = buffer.LastIndexOf('[', index0);
-                if(index1 >= 0){
-                     string payload = buffer.Substring(index1 + 1, ((index0 - index1) - 1));
-                    if(index0 == buffer.Length - 1){
-                        buffer = "";
-                    }
-                    else{
-                        buffer = buffer.Substring(index0 + 1);
-                    }
-                   // Debug.Log(payload);
-                    string[] tokens = payload.Split(' ');
-                    int x, y;// z;
-                    int.TryParse(tokens[0], out x);
-                    int.TryParse(tokens[1], out y);
-                    //int.TryParse(tokens[2], out z);
-                    if (Mathf.Abs(x) < 10) x = 0;
-                    if (Mathf.Abs(y) < 10) y = 0;
-                    velocity = (0.3f * velocity) + ((new Vector2(x, y) / 100f) * 0.7f);
-                    rigidbody.velocity = velocity;
-                    animator.SetInteger("x", x);
-                    animator.SetInteger("y", y);
-                    Debug.Log(payload);
-                    if(x > 0 && x >= y){
-                        animator.SetBool("right", true);
-                    }
-                    else if(x < 0 && x <= y){
-                        animator.SetBool("left", true);
-                    }
-                    else if(y > 0){
-                        animator.SetBool("up", true);
-                    }
-                    else if(y < 0){
-                        animator.SetBool("down", true);
-                    }
-                    else{
-                        animator.SetBool("right", false);
-                        animator.SetBool("left", false);
-                        animator.SetBool("up", false);
-                        animator.SetBool("down", false);
-                    }
-                    
-                }  
-      
+        // Update is called once per frame
+        void Update(){
+            //string state = serial.ReadLine();
+            try{
+                buffer += serial.ReadExisting();
+                //SDebug.Log(buffer);
+                int index0 = buffer.LastIndexOf(']');
+                if(index0 >= 0){
+                    int index1 = buffer.LastIndexOf('[', index0);
+                    if(index1 >= 0){
+                         string payload = buffer.Substring(index1 + 1, ((index0 - index1) - 1));
+                        if(index0 == buffer.Length - 1){
+                            buffer = "";
+                        }
+                        else{
+                            buffer = buffer.Substring(index0 + 1);
+                        }
+                       // Debug.Log(payload);
+                        string[] tokens = payload.Split(' ');
+                        int x, y;// z;
+                        int.TryParse(tokens[0], out x);
+                        int.TryParse(tokens[1], out y);
+                        //int.TryParse(tokens[2], out z);
+                        if (Mathf.Abs(x) < 10) x = 0;
+                        if (Mathf.Abs(y) < 10) y = 0;
+                        velocity = (0.3f * velocity) + ((new Vector2(x, y) / 100f) * 0.7f);
+                        rigidbody.velocity = velocity;
+                        animator.SetInteger("x", x);
+                        animator.SetInteger("y", y);
+                        Debug.Log(payload);
+                        if(x > 0 && x >= y){
+                            animator.SetBool("right", true);
+                        }
+                        else if(x < 0 && x <= y){
+                            animator.SetBool("left", true);
+                        }
+                        else if(y > 0){
+                            animator.SetBool("up", true);
+                        }
+                        else if(y < 0){
+                            animator.SetBool("down", true);
+                        }
+                        else{
+                            animator.SetBool("right", false);
+                            animator.SetBool("left", false);
+                            animator.SetBool("up", false);
+                            animator.SetBool("down", false);
+                        }
+                        
+                    }  
+          
+                }
+                else{
+                    buffer = "";
+                    animator.SetBool("right", false);
+                    animator.SetBool("left", false);
+                    animator.SetBool("up", false);
+                    animator.SetBool("down", false);
+                }
             }
-            else{
-                buffer = "";
-                animator.SetBool("right", false);
-                animator.SetBool("left", false);
-                animator.SetBool("up", false);
-                animator.SetBool("down", false);
+            catch (System.TimeoutException) {
             }
         }
-        catch (System.TimeoutException) {
+
+        void OnTriggerEnter2D(Collider2D collider){
+           Destroy(collider.gameObject);
+
         }
-    }
-
-    void OnTriggerEnter2D(Collider2D collider){
-       Destroy(collider.gameObject);
-
-    }
-    */
+        */
 }
