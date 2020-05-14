edf49321f88d5b9d83f5b9bce3d9254f78170399
xSethrin <62115192+xSethrin@users.noreply.github.com>
2020-05-13 20:18:16 -0500

    fixing Chicken and Cow stuff

---
+++ Assets/Scripts/ChickenBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* This class holds the data for Chicken Game Objects
+*
+*/
+public class ChickenBehavior : MonoBehaviour{
+    public Sprite noChicken, notFed, fed, egg;//sprites for chickens
+    public bool hasChicken = false, wasFed = false, hasEgg = false;//chicken data
+
+    /**
+    * This method is for when the chickens are clicked on.  Updates their data.
+    *
+    */
+    void OnMouseDown(){
+        if(hasChicken){//cklicking on the game object does nothing unl
+            if(!(wasFed) && JackObject.currentTool == "fodder" && JackObject.fodder > 0){//this makes sure the chicken has not been fed yet and that the player has the correct item
+                wasFed = true;
+                GetComponent<SpriteRenderer>().sprite = fed;//updates sprite
+                JackObject.fodder--;
+            }
+            if(hasEgg && JackObject.currentTool == "hand"){//this checks if the chicken laid an egg and gives one tp the player if true
+                //Adding to inventory
+                bool flag = true;
+                for (int i = 0; i < JackObject.slots.Count; i++) {//checks if egg object is already in inventory
+                    if(JackObject.slots[i].nameOf == "egg") {
+                        flag = false;
+                        JackObject.slots[i].quantity++;
+                        break;
+                    }
+                }
+                if(flag) {//if no egg objecy is in inventory
+                    JackObject.slots.Add(new Produce("egg", 80, 1, egg));
+                }
+                Debug.Log("Got Egg!");
+                hasEgg = false;
+            }       
+        }
+    }
+    
+
+    /**
+    * This method is a helper method for other classes.  It updates sprites as needed
+    *
+    */
+    public void UpdateSprite(){
+        if(hasChicken && !(wasFed)){//updates sprite if there is a chicken
+            GetComponent<SpriteRenderer>().sprite = notFed;
+        }
+        else if(hasChicken && wasFed){
+            GetComponent<SpriteRenderer>().sprite = fed;
+        }
+        else{//updates sprite if there is no chicken
+            GetComponent<SpriteRenderer>().sprite = noChicken;
+        }
+    }
+
+}
---
+++ Assets/Scripts/CowBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* This class acts as a way to hold data for cows
+* It also defines their behavior
+*/
+public class CowBehavior : MonoBehaviour{
+    public Sprite noCow, cowNotFed, cowFed, milk;//sprites for cows
+    public bool hasCow, wasFed, hasMilk;//booleans for cow data
+
+    /**
+    * This method is used to determine what happens on click of the cow objects
+    *
+    */
+    void OnMouseDown(){
+        if(hasCow){//only do stuff if there is a cow
+           if(!(wasFed) && JackObject.currentTool == "water"){//if statment to feed cow
+               GetComponent<SpriteRenderer>().sprite = cowFed;//update sprite
+               wasFed = true;
+           }
+           if(hasMilk && JackObject.currentTool == "hand"){//if statment to milk cow
+                //Adding to inventory
+                bool flag = true;
+                for (int i = 0; i < JackObject.slots.Count; i++) {
+                    if (JackObject.slots[i].nameOf == "milk") {
+                        flag = false;
+                        JackObject.slots[i].quantity++;
+                        break;
+                    }
+                }
+                if (flag) {
+                    JackObject.slots.Add(new Produce("milk", 150, 1, milk));
+                }
+                Debug.Log("Got Milk!");
+                hasMilk = false;
+           }
+        }
+    }
+
+    /**
+    * This is a helper methiod to update sprite
+    * It is used by other classes
+    */
+    public void UpdateSprite(){
+        if(hasCow && !(wasFed)){//updates if has cow
+            GetComponent<SpriteRenderer>().sprite = cowNotFed;
+        }
+        else if(hasCow && wasFed){
+            GetComponent<SpriteRenderer>().sprite = cowFed;
+        }
+        else{//update if there is no cow
+            GetComponent<SpriteRenderer>().sprite = noCow;
+        }
+
+    }
+}
