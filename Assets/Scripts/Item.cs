4be9c6976be5484b638a53fcb4b7787f3bce45f2
Sterling <Rohlinsm6519@uwec.edu>
2020-05-20 20:51:01 -0500

    Shop menus and code

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Buy_Seeds,
+        Sell_Tomatoes
+
+    }
+
+    public static int GetCost(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.Buy_Seeds: return 20;
+            case ItemType.Sell_Tomatoes: return -70; 
+        }
+    }
+
+   
+
+}
